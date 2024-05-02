986661ed0553330a98fbd3f9066ef0aa77b47785
nick3point5 <ktt4444@gmail.com>
2024-05-02 06:09:31 -0400

    renamed seed

--- src/main/java/com/store/storeAPI/api/products/ProductSeed.java
+++ src/main/java/com/store/storeAPI/api/products/ProductSeed.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ProductConfig {
+public class ProductSeed {
     @Bean
     CommandLineRunner productCommandLineRunner(ProductService productService) {
         return args -> {
